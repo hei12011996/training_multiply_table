eb84c2d475a9328e28180df18adf619c43ceb0fc
Antony Chan <antony.chan@oocl.com>
2018-11-20 17:31:13 +0800

    implement function constructMultiplyTable()

--- main.js
+++ main.js
@@ -17,6 +17,40 @@ function getMultiplyStringOfNumber(startNumber, endNumber) {
 }
 
 function constructMultiplyTable(multiplyStrings) {
+	let total = 0;
+	let outputMax = 0;
+	let multiplyTable = "";
+	let indentTable = [];
+
+	// ---Initialize indentTable to align
+	while (total < multiplyStrings.length) {
+		for (let i = 0; i <= outputMax; i++);
+		outputMax++;
+		total += outputMax;
+	}
+	for(let i = multiplyStrings.length - 1; i >= multiplyStrings.length - outputMax; i--){
+		indentTable.push(multiplyStrings[i].length + 2);
+	}
+	// --- Reverse the indentTable as it is construct backward
+	indentTable.reverse();
+
+	total = 0;
+	outputMax = 0;
+	// ---construct Multiply Table String
+	while (total < multiplyStrings.length) {
+		for (let i = 0; i <= outputMax; i++) {
+			let multiplyString = multiplyStrings[total + i];
+			if (i == outputMax) {
+				multiplyTable += multiplyString + "\n";
+			} else {
+				multiplyTable += multiplyString + " ".repeat(indentTable[i] - multiplyString.length);
+			}
+		}
+		outputMax++;
+		total += outputMax;
+	}
+
+	return multiplyTable;
 }
 
 module.exports = {
