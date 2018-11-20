cefcbe0d6729d0645d221d713c22e05dd16024bd
Antony Chan <antony.chan@oocl.com>
2018-11-20 17:30:34 +0800

    Implement function isStartEndNumberValid()

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ function getMultiplyTable(startNumber, endNumber) {
 }
 
 function isStartEndNumberValid(startNumber, endNumber) {
+	if (startNumber === null || endNumber === null) {
+		return false;
+	} else if (!(startNumber >= 1 && startNumber <= 1000)) {
+		return false;
+	} else if (!(endNumber >= 1 && endNumber <= 1000)) {
+		return false;
+	}
+	return true;
 }
 
 function getMultiplyStringOfNumber(startNumber, endNumber) {
